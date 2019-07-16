4fd836a870726a1796dc4a5f8fef515ffef4132d
vincesafe <49523796+vincesafe@users.noreply.github.com>
2019-07-16 14:51:39 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pkg install dnsutils
+pkg install coreutils
+pkg install nano
+
+# ip2ascii.sh
+wget http://vsi.us.to/send.sh
+wget http://vsi.us.to/recep.sh
+wget http://vsi.us.to/ip2ascii.sh
+wget http://vsi.us.to/rnum.sh
+
+chmod u+x *.sh
+
+
