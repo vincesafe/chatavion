1a705fa04d0f75c8aa23e7657cb5e7a49f4d753e
vincesafe <49523796+vincesafe@users.noreply.github.com>
2019-07-24 22:52:19 +0200

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SEND="emgt.xx.yy"
+
 srv=`cat dnserv`
 echo "Serveur en cours : $srv"
 echo "Pour préciser un serveur, tapez @x.x.x.x dans dnserv."
@@ -7,11 +9,15 @@ echo "En paramètre, tapez votre message entre guillemets."
 
 if [ $# -lt 1 ]
 then
-	echo "Pas de message à envoyer."
-	exit
+        echo "Pas de message à envoyer."
+        exit
 fi
 
 msg=`echo "$1" | base32`
-### REPLACE emgt.xx.yy WITH THE DOMAIN USED TO SEND MESSAGES
-dig $srv $msg.emgt.xx.yy > logdig.txt 2&>1
-echo "Tentative d'envoi. Utilisez recep.sh ou rnum.sh pour vérifier."
+dig $srv $msg.$SEND | grep 42.42.42.42  > /dev/null
+if [ $? -eq 0 ]
+then # server sends 42.42.42.42 = success
+        echo "Message envoyé avec succès."
+else
+        echo "Pas de réponse du serveur. Utilisez recepauto.sh pour vérifier."
+fi
